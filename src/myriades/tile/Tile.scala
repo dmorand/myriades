e603b2754898e6b741a3353bc715480ab609803a
David Morand <david.morand@nuecho.com>
2010-11-29 08:32:00 -0500

    Tile is now abstract

--- src/myriades/tile/Tile.scala
+++ src/myriades/tile/Tile.scala
@@ -4,11 +4,11 @@ import java.awt._
 
 import myriades.util._
 
-class Tile(id: String) extends ResourceLocator {  
+abstract class Tile(id: String) extends ResourceLocator {  
   val image = Toolkit.getDefaultToolkit.createImage(getResourceLocation(id + ".png"))
 }
 
 object Tile {
     val size = 32
 }
-
+ 
