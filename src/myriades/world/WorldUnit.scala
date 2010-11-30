809bc3085b3f4b98718ccb374245c39c96f95e3b
David Morand <david.morand@nuecho.com>
2010-11-30 17:16:28 -0500

    WorldUnit: added companion object

--- src/myriades/world/WorldUnit.scala
+++ src/myriades/world/WorldUnit.scala
@@ -6,6 +6,14 @@ import myriades.tile._
 final class WorldUnit(val background: Tile, val foreground: Tile = null) {
   def render(g: Graphics, x: Int, y: Int) = {
     g.drawImage(background.image, x * Tile.size, y * Tile.size, null)
-    g.drawImage(foreground.image, x * Tile.size, y * Tile.size, null)
+
+    if (foreground != null) {
+      g.drawImage(foreground.image, x * Tile.size, y * Tile.size, null)
+    }
   }
+}
+
+object WorldUnit {
+  def apply(background: Tile) = new WorldUnit(background)
+  def apply(background: Tile, foreground: Tile) = new WorldUnit(background, foreground)
 }
