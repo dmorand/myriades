f6ee00a81c2608770da962e425ff4a9c656097a3
David Morand <david.morand@nuecho.com>
2014-03-31 21:57:24 -0400

    improved/fixed hero displacement

--- app/dart/hero/hero-layer.dart
+++ app/dart/hero/hero-layer.dart
@@ -3,21 +3,30 @@ part of myriades.hero;
 class HeroLayer {
   CanvasElement _canvas;
   int _scale;
-  Map<Hero, Point> _heroes = {};
+  Map<Hero, Point> _positions = {};
+  Map<Point, Hero> _heroes = {};
 
   HeroLayer(this._canvas, this._scale);
 
   void moveHero(Hero hero, Point position) {
-    if(_heroes.containsKey(hero)) {
-      Point oldPosition = _heroes[hero];
+    if(_positions.containsKey(hero)) {
+      Point oldPosition = _positions[hero];
       _canvas.context2D.clearRect(oldPosition.x * TILE_SIZE * _scale, oldPosition.y * TILE_SIZE * _scale, TILE_SIZE * _scale, TILE_SIZE * _scale);
+      _heroes[oldPosition] = null;
     }
 
     hero.render(_canvas, position.x, position.y, _scale);
-    _heroes[hero] = position;
+    _heroes[position] = hero;
+    _positions[hero] = position;
   }
 
   void clear() {
     _canvas.context2D.clearRect(0, 0, ARENA_SIZE * TILE_SIZE * _scale, ARENA_SIZE * TILE_SIZE * _scale);
+    _positions.clear();
+    _heroes.clear();
+  }
+
+  Hero getHero(Point position) {
+    return _heroes[position];
   }
 }
--- app/dart/hero/hero.dart
+++ app/dart/hero/hero.dart
@@ -2,17 +2,17 @@ library myriades.hero;
 
 import 'dart:convert';
 import 'dart:html';
-import '../tile/tileset.dart';
+import '../tile/tile.dart';
 import '../arena/arena.dart';
 
 part 'hero-layer.dart';
 part 'hero-library.dart';
 
 class Hero {
-  String _id;
+  String id;
   Tile _tile;
 
-  Hero(this._id, this._tile);
+  Hero(this.id, this._tile);
 
   render(CanvasElement canvas, int x, int y, int scale) {
     _tile.render(canvas, x, y, scale);
