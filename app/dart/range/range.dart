3450afe6aa37e73ab3211e873e088b950a5308b3
David Morand <david.morand@nuecho.com>
2014-04-21 20:29:03 -0400

    range.dart: implemented all ranges

--- app/dart/range/range.dart
+++ app/dart/range/range.dart
@@ -1,70 +1,114 @@
 library myriades.range;
 
+import 'dart:collection';
 import 'dart:html';
 import '../arena/arena.dart';
 import '../tile/tile.dart';
 
 part 'range-layer.dart';
 
+Range parseRange(Map jsonRange) {
+  String type = jsonRange['type'];
+  int min = jsonRange['min'];
+  int max = jsonRange['max'];
+
+  switch(type) {
+    case 'linear': return new LinearRange(min, max);
+    case 'diagonal': return new DiagonalRange(min, max);
+    case 'radius': return new RadiusRange(min, max);
+    case 'diamond': return new DiamondRange(min, max);
+  }
+
+  throw 'Unsupported range type: "$type"';
+}
+
 abstract class Range {
   int min;
   int max;
 
-  List<Point> getArea(Point position);
+  Range(this.min, this.max);
+
+  Set<Point> getArea(Point position);
 }
 
-bool isValid(Point position) {
+class LinearRange extends Range {
+  LinearRange(int min, int max) : super(min, max);
+
+  Set<Point> getArea(Point position) {
+    Set<Point> area = new HashSet<Point>();
+
+    for(int i = this.min; i <= this.max; i++) {
+      _addPosition(new Point(position.x + i, position.y), area);
+      _addPosition(new Point(position.x - i, position.y), area);
+      _addPosition(new Point(position.x, position.y + i), area);
+      _addPosition(new Point(position.x, position.y - i), area);
+    }
+
+    return area;
+  }
+}
+
+class DiagonalRange extends Range {
+  DiagonalRange(int min, int max) : super(min, max);
+
+  Set<Point> getArea(Point position) {
+    Set<Point> area = new HashSet<Point>();
+
+    for(int i = this.min; i <= this.max; i++) {
+      _addPosition(new Point(position.x + i, position.y + i), area);
+      _addPosition(new Point(position.x + i, position.y - i), area);
+      _addPosition(new Point(position.x - i, position.y + i), area);
+      _addPosition(new Point(position.x - i, position.y - i), area);
+    }
+
+    return area;
+  }
+}
+
+class RadiusRange extends Range {
+  RadiusRange(int min, int max) : super(min, max);
+
+  Set<Point> getArea(Point position) {
+    Set<Point> area = new HashSet<Point>();
+
+    for(int i = this.min; i <= this.max; i++) {
+      for(int delta = -i; delta <= i; delta++) {
+        _addPosition(new Point(position.x + i, position.y + delta), area);
+        _addPosition(new Point(position.x - i, position.y + delta), area);
+        _addPosition(new Point(position.x + delta, position.y + i), area);
+        _addPosition(new Point(position.x + delta, position.y - i), area);
+      }
+    }
+
+    return area;
+  }
+}
+
+class DiamondRange extends Range {
+  DiamondRange(int min, int max) : super(min, max);
+
+  Set<Point> getArea(Point position) {
+    Set<Point> area = new HashSet<Point>();
+
+    for(int x = -this.max; x <= this.max; x++) {
+      for(int y = -this.max; y <= this.max; y++) {
+        num sum = x.abs() + y.abs();
+        if(sum >= this.min && sum <= this.max) {
+          _addPosition(new Point(position.x + x, position.y + y), area);
+        }
+      }
+    }
+
+    return area;
+  }
+}
+
+bool _isValid(Point position) {
   return position.x >= 0 && position.x < ARENA_SIZE && position.y >= 0 && position.y < ARENA_SIZE;
 }
 
-void addPosition(Point position, List<Point> area) {
-  if(isValid(position)) {
+void _addPosition(Point position, Set<Point> area) {
+  if(_isValid(position)) {
     area.add(position);
-  }
-}
-
-class Linear extends Range {
-  List<Point> getArea(Point position) {
-    List<Point> area = [];
-
-    for(int i = this.min; i <= this.max; i++) {
-      addPosition(new Point(position.x + i, position.y), area);
-      addPosition(new Point(position.x - i, position.y), area);
-      addPosition(new Point(position.x, position.y + i), area);
-      addPosition(new Point(position.x, position.y - i), area);
-    }
-
-    return area;
-  }
-}
-
-class Diagonal extends Range {
-  List<Point> getArea(Point position) {
-    List<Point> area = [];
-
-    for(int i = this.min; i <= this.max; i++) {
-      addPosition(new Point(position.x + i, position.y + i), area);
-      addPosition(new Point(position.x + i, position.y - i), area);
-      addPosition(new Point(position.x - i, position.y + i), area);
-      addPosition(new Point(position.x - i, position.y - i), area);
-    }
-
-    return area;
-  }
-}
-
-class Radius extends Range {
-  List<Point> getArea(Point position) {
-    List<Point> area = [];
-
-    return area;
-  }
-}
-
-class Diamond extends Range {
-  List<Point> getArea(Point position) {
-    List<Point> area = [];
-
-    return area;
   }
 }
